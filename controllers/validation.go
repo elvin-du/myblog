a790a3442403cf701a393d1a0160c583a383d5e7
dumx <dumx@dumx-ThinkPad-SL410.(none)>
2013-05-28 08:55:27 +0800

    add cookie feature

--- controllers/validation.go
+++ controllers/validation.go
@@ -4,7 +4,13 @@ import(
 	"myblog/models"
 )
 
+
 func CheckNamePsw(name,psw string)error{
 	medol := models.Model{}
 	return medol.CheckNamePsw(name,psw)
+}
+
+func CreateSession()int{
+
+	return -1	
 }
